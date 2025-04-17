414054bad79564fac6e349d6c8448b2f5dc0db57
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-04-17 18:39:51 +0600

    Solving the deployment Issue in the server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const defaultgameCollection = client.db("GameHeist").collection("default");
     const gameListCollection = client.db("GameHeist").collection("gamelist");
@@ -221,7 +221,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
